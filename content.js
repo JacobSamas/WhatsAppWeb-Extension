c5bb4b344e18a4b866a450a39adec05257c7e20c
Jacob Samas <samasjacob@icloud.com>
2024-05-05 19:43:20 +0530

    feat: add json

---
+++ content.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'displayContacts') {
+        displayContacts(message.contacts);
+    }
+});
+
+function displayContacts(contacts) {
+   
+    console.log('Displaying contacts:', contacts);
+}
